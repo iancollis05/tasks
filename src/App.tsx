bae6958a500e27754914bc67c35a71dc33b8ceeb
iancollis05 <icollis@udel.edu>
2026-02-03 14:19:41 -0500

    removed a little bit of text / retest

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,7 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript (Ian Collis)
             </header>
-            <p>
-                Ian Collis (had to add another one its been a minute since i
-                used html)
-            </p>
+            <p>Ian Collis</p>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
