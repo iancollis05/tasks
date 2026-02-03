f085ae4bb1e9f22f1d5fb63a0e7d898cdd1b8df1
iancollis05 <icollis@udel.edu>
2026-02-03 14:15:25 -0500

    Added name to app

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,12 @@ function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                UD CISC275 with React Hooks and TypeScript (Ian Collis)
             </header>
+            <p>
+                Ian Collis (had to add another one its been a minute since i
+                used html)
+            </p>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
