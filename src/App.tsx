3b96445073ceeb36f955550d9fe8878b2074d278
iancollis05 <icollis@udel.edu>
2026-02-06 15:32:12 -0500

    Completed HTML and CSS Tasks / played around a little

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import "./App.css";
 
 function App(): React.JSX.Element {
@@ -7,8 +9,65 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript (Ian Collis)
             </header>
+            <h1>Chat is this header fire</h1>
+
+            <div>
+                This is <span style={{ color: "blue" }}> colored text</span>{" "}
+                (not a hyperlink.)
+            </div>
+
+            <div>
+                <Container>
+                    <Row>
+                        <Col>
+                            First column.
+                            <div
+                                style={{
+                                    display: "flex",
+                                    width: "100px",
+                                    height: "50px",
+                                    backgroundColor: "red",
+                                    justifyContent: "center",
+                                }}
+                            ></div>
+                            <p> </p>
+                        </Col>
+
+                        <Col>
+                            Second column.
+                            <div
+                                style={{
+                                    width: "100px",
+                                    height: "50px",
+                                    backgroundColor: "red",
+                                }}
+                            ></div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+
             <p>Ian Collis</p>
+            <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR88UGwbxf1IIprd3C5MtEZ6U5gq5xNyVFAKA&s"
+                alt="How I feel being in ROTC"
+            />
+
             <p>Hello World!</p>
+            <p> Is this a W list</p>
+            <ol>
+                <li>Hello World!</li>
+                <li>Hello Dr. Bart!</li>
+                <li>Hello TAs!</li>
+            </ol>
+
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
 
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
