7c77b6436111f29f6dd884c408ce13771e9f7513
iancollis05 <icollis@udel.edu>
2026-03-01 14:35:33 -0500

    objects complete

--- src/objects.ts
+++ src/objects.ts
@@ -77,7 +77,11 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    let markdown: string = "# " + question.name + "\n" + question.body;
+    if (question.type === "multiple_choice_question") {
+        markdown += "\n- " + question.options.join("\n- ");
+    }
+    return markdown;
 }
 
 /**
@@ -85,7 +89,10 @@ export function toMarkdown(question: Question): string {
  * `newName`.
  */
 export function renameQuestion(question: Question, newName: string): Question {
-    return Question{
+    return {
+        ...question,
+        name: newName,
+    };
 }
 
 /**
@@ -94,8 +101,7 @@ export function renameQuestion(question: Question, newName: string): Question {
  * published; if it was published, now it should be not published.
  */
 export function publishQuestion(question: Question): Question {
-    
-    return question;
+    return { ...question, published: !question.published };
 }
 
 /**
@@ -105,7 +111,12 @@ export function publishQuestion(question: Question): Question {
  * The `published` field should be reset to false.
  */
 export function duplicateQuestion(id: number, oldQuestion: Question): Question {
-    return oldQuestion;
+    return {
+        ...oldQuestion,
+        id: id,
+        name: "Copy of " + oldQuestion.name,
+        published: false,
+    };
 }
 
 /**
@@ -116,7 +127,10 @@ export function duplicateQuestion(id: number, oldQuestion: Question): Question {
  * Check out the subsection about "Nested Fields" for more information.
  */
 export function addOption(question: Question, newOption: string): Question {
-    return question;
+    return {
+        ...question,
+        options: [...question.options, newOption],
+    };
 }
 
 /**
@@ -133,5 +147,11 @@ export function mergeQuestion(
     contentQuestion: Question,
     { points }: { points: number },
 ): Question {
-    return contentQuestion;
+    return {
+        ...contentQuestion,
+        id: id,
+        name: name,
+        points: points,
+        published: false,
+    };
 }
